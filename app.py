f6e5f451153e83cbd51098a13c453b682aad5b63
Navya <75139913+rajputnavya@users.noreply.github.com>
2023-07-02 05:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-from predict_page import show_predict_page
-from explore_page import show_explore_page
+from predict import show_predict_page
+from explore import explore_page
 
+page_Selection = st.sidebar.selectbox("Explore Or Predict" , ("Predict" , "Explore"))
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
-
-if page == "Predict":
+if page_Selection == "Predict":
     show_predict_page()
 else:
-    show_explore_page()
+    explore_page()
